38f0dcee9b7f60d2ec5de7c02c8bd536148e63da
your_github_matheus-sousa1 <root@LAPTOP-JA2RMI7K.localdomain>
2022-12-17 16:49:31 -0300

    commit aumentar quantidade de items

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -1,7 +1,11 @@
 import styles from "./cart.css";
+import { useContext } from "react";
+import { ShopContext } from "../../context/shop_context";
 
 export const CartItem = (props) => {
   const { id, productName, price, productImage } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+    useContext(ShopContext);
 
   return (
     <div className="cartItem">
@@ -11,6 +15,14 @@ export const CartItem = (props) => {
           <b>{productName}</b>
         </p>
         <p>R$ {price}</p>
+        <div className="countHandler">
+          <button onClick={() => removeFromCart(id)}> - </button>
+          <input
+            value={cartItems[id]}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+          />
+          <button onClick={() => addToCart(id)}> + </button>
+        </div>
       </div>
     </div>
   );
--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,9 +3,14 @@ import { ShopContext } from "../../context/shop_context";
 import { useContext } from "react";
 import { CartItem } from "./cart-item";
 import styles from "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 export const Cart = () => {
-  const { cartItems } = useContext(ShopContext);
+  const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+  const totalAmount = getTotalCartAmount();
+
+  const navigate = useNavigate();
+
   return (
     <div className="cart">
       <div>
@@ -17,6 +22,11 @@ export const Cart = () => {
             return <CartItem data={product} />;
           }
         })}
+      </div>
+      <div className="checkout">
+        <p>Subtotal: R${totalAmount}</p>
+        <button onClick={() => navigate("/")}>Continuar comprando</button>
+        <button>Conferir</button>
       </div>
     </div>
   );
